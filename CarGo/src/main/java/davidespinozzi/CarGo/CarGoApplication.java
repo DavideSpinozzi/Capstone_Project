45025b80932afad6188e8a326ae3d14e6bbb099a
Davide Spinozzi <davidespinozzi01@gmail.com>
2023-09-05 14:54:22 +0200

    .

---
+++ CarGo/src/main/java/davidespinozzi/CarGo/CarGoApplication.java
@@ -0,0 +1,13 @@
+package davidespinozzi.CarGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarGoApplication.class, args);
+	}
+
+}
---
+++ CarGo/src/test/java/davidespinozzi/CarGo/CarGoApplicationTests.java
@@ -0,0 +1,13 @@
+package davidespinozzi.CarGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
